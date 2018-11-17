7e72c582b68481d7142386880c2d8c038e108aa4
arshad <mdarshadrashid98@gmail.com>
2018-11-17 11:46:27 +0530

    3rd commit

---
+++ login_form.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+    <head>
+        <title> login form </title>
+        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.css" type="text/css" />
+        
+    </head>
+    <body>
+        <center><h1> LOGIN FORM </h1></center>
+            <hr width="50%" color="green">
+        <center>                               
+            <form method="POST" action="login_submit.php">
+                </div class="form-group">
+                    EMAIL 
+                    <input type="text" class="form-control" placeholder="email" name="email"/>
+                </div>
+                
+                <div class="form-group"> 
+                    PASSWORD
+                    <input type="password" class="form-control" placeholder="password" name="password"/>
+                </div>
+                    <br/><br/>
+                
+                <center> <button type="submit" class="btn-lg btn-success">login</button> </center>
+            </form>  
+        </center>   
+   </body>
+</html>
